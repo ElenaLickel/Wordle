240f422f6813226beb3f2283288d03306d13523d
ElenaLickel <elickel.ieu2020@student.ie.edu>
2022-03-25 12:47:08 +0100

    while true loop for letter and word check

--- game.py
+++ game.py
@@ -1,10 +1,23 @@
-game_word = 'hello'
+
+game_word = 'carlo'
 game_word = list(game_word)
 print(game_word)
-guess = list(input('Guess: '))
-print(f'your guess was{guess}')
 
-if guess == game_word:
-    print('correct')
+while True:
+    guess = list(input('Guess: '))
+    print(f'your guess was {guess}')
+
+    if guess == game_word:
+        print('correct')
+        break
+    else: print('still wrong word')
+
+    for i in range(len(guess)):
+        if guess[i] == game_word[i]:
+            print(f'correct letter {guess[i]} and position')
+        elif guess[i] in game_word:
+            print(f'correct letter {guess[i]}')
+        else: print(f'wrong letter {guess[i]}')
+
 
 
